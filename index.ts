d77b9d8a32755764f826ad3cb02a968037ce9912
Bret Hudson <bret@brethudson.com>
2025-11-16 00:18:00 -0800

    Added tests for clamp() and lerp(), removed them from global Math

--- index.ts
+++ index.ts
@@ -4,26 +4,14 @@ export * from './vectors/index.js';
 
 export const EPSILON = 0.000001;
 
-declare global {
-	interface Math {
-		clamp: (val: number, min: number, max: number) => number;
-		lerp: (a: number, b: number, t: number) => number;
-	}
+export function clamp(val: number, min: number, max: number) {
+	if (val < min) return min;
+	if (val > max) return max;
+	return val;
 }
 
-// Math prototype fun :~)
-if (typeof Math.clamp === 'undefined') {
-	Math.clamp = (val, min, max) => {
-		if (val < min) return min;
-		if (val > max) return max;
-		return val;
-	};
-}
-
-if (typeof Math.lerp === 'undefined') {
-	Math.lerp = (a, b, t) => {
-		return t * (b - a) + a;
-	};
+export function lerp(a: number, b: number, t: number) {
+	return t * (b - a) + a;
 }
 
 export type Line2D = [Vec2, Vec2];
