682455dac7201b4040fe74e65e4a9b6fcfb8886d
Bret Hudson <bret@brethudson.com>
2025-12-17 15:20:11 -0600

    Added coverage to vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
+		coverage: {
+			cleanOnRerun: true,
+			allowExternal: true,
+			provider: 'v8',
+			exclude: ['setup/matchers/*'],
+		},
 		root: 'tests',
 		setupFiles: ['setup/index.setup.ts'],
 	},
